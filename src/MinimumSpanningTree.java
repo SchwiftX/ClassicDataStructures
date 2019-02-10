9768588774568931064d2d98f4ac1aac33ae6573
SchwiftX <2621890948@qq.com>
2019-02-10 00:47:11 -0600

    UnionFindSetEnhanced

--- src/MinimumSpanningTree.java
+++ src/MinimumSpanningTree.java
@@ -5,39 +5,55 @@ public class MinimumSpanningTree { // Minimum Spanning Tree
     如果说可以将所有城市都连接起来，则返回这个连接方法；不然的话返回一个空列表。*/
     public List<Connection> lowestCost(List<Connection> connections) {
         List<Connection> res = new ArrayList<>();
-        UnionFindSet ufs = new UnionFindSet(connections.size() * 2); // 最多可能有connections长度2倍的点，但是不一定全都会用到。
         Collections.sort(connections, new Comparator<Connection>() {
             @Override
             public int compare(Connection conn1, Connection conn2) {
-                if(conn1.cost != conn2.cost)
+                if (conn1.cost != conn2.cost)
                     return conn1.cost - conn2.cost;
                 return (conn1.city1 + conn1.city2).compareTo(conn2.city1 + conn2.city2);
             }
-        });// TODO: 2019/2/9
+        });
+
+        // UnionFindSetEnhanced 解法
+        Map<String, UnionFindSetEnhanced> cityMap = new HashMap<>();
+        for (Connection c : connections) {
+            UnionFindSetEnhanced ufs1 = cityMap.getOrDefault(c.city1, new UnionFindSetEnhanced());
+            UnionFindSetEnhanced ufs2 = cityMap.getOrDefault(c.city2, new UnionFindSetEnhanced());
+            cityMap.put(c.city1, ufs1);
+            cityMap.put(c.city2, ufs2);
+            if(ufs1.find() != ufs2.find()){
+                ufs1.union(ufs2);
+                res.add(c);
+            }
+        }
+
+        /*
+        //UnionFindSet的解法
         Map<String, Integer> cityMap = new HashMap<>();
+        UnionFindSet ufs = new UnionFindSet(connections.size() * 2); // 最多可能有connections长度2倍的点，但是不一定全都会用到。
         for (Connection c : connections) {
             int c1 = getCityId(cityMap, c.city1);
             int c2 = getCityId(cityMap, c.city2);
-            if(ufs.find(c1) != ufs.find(c2)){ // 表示两个城市尚未联通
+            if (ufs.find(c1) != ufs.find(c2)) { // 表示两个城市尚未联通
                 ufs.union(c1, c2); // 联通
                 res.add(c); // 记入结果
             }
-        }
-        if(res.size() + 1 == cityMap.size()) // 恰好联通所有城市且没有环路时，道路数目+1 = 城市数量
+        }*/
+
+        if (res.size() + 1 == cityMap.size()) // 恰好联通所有城市且没有环路时，道路数目+1 = 城市数量
             return res;
         return new ArrayList<>();
     }
 
     private int getCityId(Map<String, Integer> cityMap, String cityName) {
         int id = cityMap.getOrDefault(cityName, -1);
-        if(id == -1){
+        if (id == -1) {
             id = cityMap.size();
             cityMap.put(cityName, id);
         }
         return id;
     }
 }
-
 
 
 class Connection {
@@ -81,5 +97,36 @@ class UnionFindSet { //根节点为代表元素，其值是负数，负几表示
             return target;
         set[target] = find(set[target]);
         return set[target];
+    }
+}
+
+class UnionFindSetEnhanced { // UnionFindSet的增强 不限长度，城市数量不用预先定死
+    int count;
+    UnionFindSetEnhanced father;
+
+    public UnionFindSetEnhanced() {
+        count = 1;
+        father = this;
+    }
+
+    public void union(UnionFindSetEnhanced ufsb) {
+        UnionFindSetEnhanced ufsa = find();
+        ufsb = ufsb.find();
+        if (ufsa.count > ufsb.count) { //说明集合A更大
+            ufsa.count += ufsb.count;
+            ufsb.count = 1;
+            ufsb.father = ufsa;
+        } else {
+            ufsb.count += ufsa.count;
+            ufsa.count = 1;
+            ufsa.father = ufsb;
+        }
+    }
+
+    public UnionFindSetEnhanced find() {
+        if (this == father)
+            return this;
+        father = father.find();
+        return father;
     }
 }
